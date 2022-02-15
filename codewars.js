f85f137d5a2e6367ffacc92d5ef33a571eb21745
vitaliy-1111 <networkvetalius@gmail.com>
2022-02-15 21:33:30 +0200

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -23,3 +23,9 @@
 //   : s.slice(s.length/2, s.length/2 + 1 )
 // }
 // console.log(getMiddle('123456'))
+
+// function reverseWords(str) {
+//   return str.split(' ').map(a => a.split('').reverse().join('')).join(' ');
+
+// }
+// console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
